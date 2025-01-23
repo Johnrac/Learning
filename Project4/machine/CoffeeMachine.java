f5543670035cac6a9ee136a74a028940977d7f61
Johnrac <johnracer@mail.ru>
2025-01-24 00:26:44 +1000

    Stage 4 Add Functions

--- Project4/machine/CoffeeMachine.java
+++ Project4/machine/CoffeeMachine.java
@@ -4,69 +4,126 @@ import java.util.Scanner;
 
 public class CoffeeMachine {
 
-    private static final int WATER_ONE_CUP = 200;
-    private static final int MILK_ONE_CUP = 50;
-    private static final int COFFEE_ONE_CUP = 15;
-    private static final int[] requiredIngridientsOneCup = { WATER_ONE_CUP, MILK_ONE_CUP, COFFEE_ONE_CUP };
-    private static final String[] ingridientRequests = {
-            "Write how many ml of water the coffee machine has:",
-            "Write how many ml of milk the coffee machine has:",
-            "Write how many grams of coffee beans the coffee machine has:"
-    };
-    private static final String INGRIDIENTS_ENOUGH = "Yes, I can make that amount of coffee";
-    private static final String INGRIDIENTS_MORE = "Yes, I can make that amount of coffee (and even %d more than that)";
-    private static final String INGRIDIENTS_NOT_ENOUGH = "No, I can make only %d cup(s) of coffee";
+    private static final String CAPPUCCINO = "cappuccino";
+    private static final String LATTE = "latte";
+    private static final String ESPRESSO = "espresso";
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    private static int countWater = 400;
+    private static int countMoney = 550;
+    private static int countMilk = 540;
+    private static int countCoffee = 120;
+    private static int countCups = 9;
 
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
-        
-        int[] ingridients = getIngridients(scanner);
+        outputInformation();
 
-        int requestedCountCups = requestCountCups(scanner);
+        System.out.println();
 
-        int currentCountCups = calculateCupsFrom(ingridients);
+        selectAction();
 
-        String message = getMessage(requestedCountCups, currentCountCups);
+        System.out.println();
 
-        System.out.println(message);
+        outputInformation();
 
         scanner.close();
 
     }
 
-    private static String getMessage(int requestedCountCups, int currentCountCups) {
-        String message = INGRIDIENTS_NOT_ENOUGH;
-
-        if (currentCountCups >= requestedCountCups) {
-            message = currentCountCups == requestedCountCups ? INGRIDIENTS_ENOUGH : INGRIDIENTS_MORE;
-            currentCountCups -= requestedCountCups;
-        }
-        return String.format(message, currentCountCups);
+    private static void outputInformation() {
+        System.out.printf("""
+                The coffee machine has:
+                %d ml of water
+                %d ml of milk
+                %d g of coffee beans
+                %d disposable cups
+                $%d of money
+                """, countWater, countMilk, countCoffee, countCups, countMoney);
     }
 
-    private static int calculateCupsFrom(int[] ingridients) {
-        int minCountCups = Integer.MAX_VALUE;
-        for (int i = 0; i < ingridients.length; i++) {
-            minCountCups = Math.min(minCountCups, ingridients[i] / requiredIngridientsOneCup[i]);
+    private static void selectAction() {
+        System.out.println("Write action (buy, fill, take):");
+        switch (scanner.nextLine()) {
+            case "buy" -> buyCoffee();
+            case "fill" -> fillIngridients();
+            case "take" -> takeMoney();
+            default -> throw new UnsupportedOperationException("Action may be only buy, fill or take");
         }
-        return minCountCups;
     }
 
-    private static int requestCountCups(Scanner scanner) {
-        System.out.println("Write how many cups of coffee you will need:");
-        return scanner.nextInt();
+    private static void buyCoffee() {
+        String coffee = chooseCoffee();
+        getMoneyFor(coffee);
+        makeCoffee(coffee);
     }
 
-    private static int[] getIngridients(Scanner scanner) {
-
-        int[] ingridients = new int[ingridientRequests.length];
-
-        for (int i = 0; i < ingridients.length; i++) {
-            System.out.println(ingridientRequests[i]);
-            ingridients[i] = scanner.nextInt();
+    private static void getMoneyFor(String coffee) {
+        switch (coffee) {
+            case ESPRESSO -> countMoney += 4;
+            case LATTE -> countMoney += 7;
+            case CAPPUCCINO -> countMoney += 6;
         }
+    }
 
-        return ingridients;
+    private static void makeCoffee(String coffee) {
+        switch (coffee) {
+            case ESPRESSO -> makeEspresso();
+            case LATTE -> makeLatte();
+            case CAPPUCCINO -> makeCapuccino();
+        }
+    }
+
+    private static void makeEspresso() {
+        countWater -= 250;
+        countCoffee -= 16;
+        countCups--;
+    }
+
+    private static void makeLatte() {
+        countWater -= 350;
+        countMilk -= 75;
+        countCoffee -= 20;
+        countCups--;
+    }
+
+    private static void makeCapuccino() {
+        countWater -= 200;
+        countMilk -= 100;
+        countCoffee -= 12;
+        countCups--;
+    }
+
+    private static String chooseCoffee() {
+        System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:");
+        var bufScanner = new Scanner(scanner.nextLine());
+        String coffee = switch (bufScanner.nextInt()) {
+            case 1 -> ESPRESSO;
+            case 2 -> LATTE;
+            case 3 -> CAPPUCCINO;
+            default -> {
+                bufScanner.close();
+                throw new UnsupportedOperationException("You should select one of 3 coffees");
+            }
+        };
+        bufScanner.close();
+        return coffee;
+    }
+
+    private static void fillIngridients() {
+        System.out.println("Write how many ml of water you want to add:");
+        countWater += scanner.nextInt();
+        System.out.println("Write how many ml of milk you want to add:");
+        countMilk += scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans you want to add:");
+        countCoffee += scanner.nextInt();
+        System.out.println("Write how many disposable cups you want to add:");
+        countCups += scanner.nextInt();
+    }
+
+    private static void takeMoney() {
+        System.out.println("I gave you $" + countMoney);
+        countMoney = 0;
     }
 }
